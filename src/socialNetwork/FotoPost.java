3c52960cf3d6c53fe8e556e0c54cbcf089c131af
leeonwxlfHTL <leon.wolf@hak-reutte.ac.at>
2024-02-06 13:14:28 +0100

    added logic

--- src/socialNetwork/FotoPost.java
+++ src/socialNetwork/FotoPost.java
@@ -1,5 +1,20 @@
 package socialNetwork;
 
-public class FotoPost {
+public class FotoPost extends Post{
 
+	private String linkToPicture;
+	
+	public FotoPost(String publisher, String title, int numOfLikes, String linkToPicture) {
+		super(publisher, title, numOfLikes);
+		this.linkToPicture = linkToPicture; 
+	}
+
+	public String getLinkToPicture() {
+		return linkToPicture;
+	}
+
+	public void setLinkToPicture(String linkToPicture) {
+		this.linkToPicture = linkToPicture;
+	}
+	
 }
