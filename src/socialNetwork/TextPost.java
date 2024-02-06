07070619d463bc147dbe037285240b7fcb21d924
leeonwxlfHTL <leon.wolf@hak-reutte.ac.at>
2024-02-06 13:14:17 +0100

    added the logic

--- src/socialNetwork/TextPost.java
+++ src/socialNetwork/TextPost.java
@@ -1,5 +1,21 @@
 package socialNetwork;
 
-public class TextPost {
+public class TextPost extends Post{
+	
+	private String message;
 
+	public TextPost(String publisher, String title, int numOfLikes, String message) {
+		super(publisher, title, numOfLikes);
+		this.message =  message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	
 }
